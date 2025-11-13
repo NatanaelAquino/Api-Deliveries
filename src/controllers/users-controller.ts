bfda10357b9684d2c2f86f39421e3c6a3f56d5e1
Natanael Aquino <wedit62@gmail.com>
2025-11-13 15:52:41 -0300

    Criado banco de dados e o rota de create user

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from 'express';
-
+import { z} from "zod"
 class UserController {
-    createUser(req: Request, res: Response): Response {
-        return res.status(201).send({ message: 'User created successfully' });
-    }
+    createUser(req: Request, res: Response ) {
+        
+        const createUserBody = z.object({
+            name: z.string().trim().min(2),
+            email: z.string().email(),
+            password: z.string().min(6)
+        })
+
+        const { name, email, password } = createUserBody.parse(req.body)
+
+        return res.status(201).json({ message: 'User created successfully', data: { name, email } });
+        
+    }   
 }
 
 export { UserController };
--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -5,6 +5,6 @@ import { UserController } from "../controllers/users-controller";
 const usersRoutes = Router();
 const userController = new UserController();
 
-usersRoutes.get("/users", userController.createUser);
+usersRoutes.post("/users", userController.createUser);
 
-export { usersRoutes };
+export { usersRoutes }; 
