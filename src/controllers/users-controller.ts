f48f21e1949c7c37ddc79fbb7243440754b8ca6d
Natanael Aquino <wedit62@gmail.com>
2025-11-13 16:10:06 -0300

    Foi realizada a criptografia da senha e a terminada a rota para create user

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
-import { z} from "zod"
+import { prisma } from '../database/prisma';
+import { AppError } from '@/utils/AppError';
+import { z } from "zod"
+import { hash } from 'bcrypt';
+
 class UserController {
-    createUser(req: Request, res: Response ) {
-        
+    async createUser(req: Request, res: Response) {
+
         const createUserBody = z.object({
             name: z.string().trim().min(2),
             email: z.string().email(),
@@ -11,9 +15,25 @@ class UserController {
 
         const { name, email, password } = createUserBody.parse(req.body)
 
-        return res.status(201).json({ message: 'User created successfully', data: { name, email } });
-        
-    }   
+        const userWithSameEmail = await prisma.user.findFirst({ where: { email } })
+
+        if (userWithSameEmail) {
+            throw new AppError('Email already in use', 409);
+        }
+
+        const hashedPassword = await hash(password, 8)
+
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword
+            }
+        });
+        const {password: _, ...userWhithoutPassord} = user;
+        return res.status(201).json({ message: 'User created successfully', userWhithoutPassord });
+
+    }
 }
 
 export { UserController };
